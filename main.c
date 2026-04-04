b9eaadc7279c53ea74a96ca3d1019ee760b97db1
Bogdan <Bogdan4ik0759@gmail.com>
2026-04-05 01:12:24 +0300

    reboot shutdown

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 
 int lm(int argc, char *argv[]);
+int rb(int argc, char *argv[]);
+int sd(int argc, char *argv[]);
 char version[32] = "0.2";
 
 void show_help() {
@@ -10,6 +12,8 @@ void show_help() {
     printf("\n");
     printf("available tools:\n");
     printf("  lastmod - print the last modification time of a file\n");
+    printf("  reboot - reboot the system (optional: -t seconds)\n");
+    printf("  shutdown - power off the system (optional: -t seconds)\n");
 }
 
 
@@ -21,6 +25,12 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(argv[1], "lastmod") == 0) {
         return lm(argc - 1, argv + 1);
+    }
+    if (strcmp(argv[1], "reboot") == 0) {
+        return rb(argc - 1, argv + 1);
+    }
+    if (strcmp(argv[1], "shutdown") == 0) {
+        return sd(argc - 1, argv + 1);
     }
 
     if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0 ||
